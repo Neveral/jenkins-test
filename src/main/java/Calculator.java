4bc81b86c4096173a52cc77a0abac4a9dc4a6180
Georgy Strelchenok <georgy.strelchenok@gmail.com>
2017-09-14 19:41:08 +0300

    init commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Neveral on 14/09/2017.
+ */
+public class Calculator {
+    public static void main(String[] args) {
+
+    }
+
+    public long calculateSum(int a, int b) {
+        return a+b;
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by Neveral on 14/09/2017.
+ */
+public class CalculatorTest {
+    Calculator calculator;
+    
+    @Before
+    public void init() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void calculateSumTest() {
+        long sum = calculator.calculateSum(4, 2);
+        assertEquals(6, sum);
+    }
+
+}
