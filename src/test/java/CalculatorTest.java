5b38cb30c33d6bba081350c8918f1f151c8fd1aa
Georgy Strelchenok <georgy.strelchenok@gmail.com>
2017-09-14 23:06:57 +0300

    change test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -17,7 +17,7 @@ public class CalculatorTest {
     @Test
     public void calculateSumTest() {
         long sum = calculator.calculateSum(4, 2);
-        assertEquals(4, sum);
+        assertEquals(6, sum);
     }
 
 }
